3324debe5b2def1a0796e4526f1cf497e525cebb
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-05 10:46:47 +0530

    init

---
+++ app/src/main/java/com/nishiket/task/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nishiket.task
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.nishiket.task.activity.TaskOfActivity
+import com.nishiket.task.layouts.LayoutsActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var acitivity_task: Button
+    private lateinit var layouts_task: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findIds()
+        val activity: Intent = Intent(this, TaskOfActivity::class.java)
+        acitivity_task.setOnClickListener{ view->
+            startActivity(activity)
+        }
+        val layouts: Intent = Intent(this, LayoutsActivity::class.java)
+        layouts_task.setOnClickListener{ view->
+            startActivity(layouts)
+        }
+    }
+
+    fun findIds() {
+        acitivity_task = findViewById(R.id.activity_task)
+        layouts_task = findViewById(R.id.layout_task)
+    }
+
+}
