5a75caa95cdbfc11de905681341dab19df3f7d66
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-06 19:06:39 +0530

    partially dimension task completed

---
+++ app/src/main/java/com/nishiket/task/dimension/DimensionActivity.kt
@@ -0,0 +1,15 @@
+package com.nishiket.task.dimension
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nishiket.task.R
+
+class DimensionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dimension)
+    }
+}
