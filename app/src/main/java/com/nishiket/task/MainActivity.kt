6ddc8bac9bde3a293739033545d291122198beae
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-07 17:30:40 +0530

    view pager task activity and packgae created,button added MainActivity

--- app/src/main/java/com/nishiket/task/MainActivity.kt
+++ app/src/main/java/com/nishiket/task/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.nishiket.task.activity.TaskOfActivity
 import com.nishiket.task.drawable.DrawableActivity
 import com.nishiket.task.layouts.LayoutsActivity
+import com.nishiket.task.viewpager.ViewPagerActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var acitivity_task: Button
     private lateinit var layouts_task: Button
     private lateinit var drawable_task: Button
+    private lateinit var viewpager_task: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,12 +30,17 @@ class MainActivity : AppCompatActivity() {
         drawable_task.setOnClickListener{ view->
             startActivity(drawable)
         }
+        val viewpager: Intent = Intent(this, ViewPagerActivity::class.java)
+        viewpager_task.setOnClickListener{ view->
+            startActivity(viewpager)
+        }
     }
 
     fun findIds() {
         acitivity_task = findViewById(R.id.activity_task)
         layouts_task = findViewById(R.id.layout_task)
         drawable_task = findViewById(R.id.drawable_task)
+        viewpager_task = findViewById(R.id.viewpager_task)
     }
 
 }
