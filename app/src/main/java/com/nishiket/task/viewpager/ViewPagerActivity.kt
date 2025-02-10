323ff70600ea1fd59244fec25ef5c20b0694c23c
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-10 14:19:02 +0530

    Infinite scroll is implemented

--- app/src/main/java/com/nishiket/task/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/nishiket/task/viewpager/ViewPagerActivity.kt
@@ -1,6 +1,7 @@
 package com.nishiket.task.viewpager
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -15,15 +16,44 @@ class ViewPagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_pager)
+
+        // all images of evey category
         val animeWallpaper = listOf(R.drawable.anime1,R.drawable.anime2,R.drawable.anime3,R.drawable.anime4,R.drawable.anime5,R.drawable.anime6)
         val natureWallpaper = listOf(R.drawable.nature1,R.drawable.nature2,R.drawable.nature3,R.drawable.nature4,R.drawable.nature5)
         val carsWallpaper = listOf(R.drawable.car1,R.drawable.car2,R.drawable.car3,R.drawable.car4)
+
+        // category list
         val category = listOf("Anime","Cars","Nature")
+
+        // category fragments object
         val fragments = listOf(AnimeFragment(animeWallpaper),CarsFragment(carsWallpaper),NatureFragment(natureWallpaper))
+
+        // adapter object, setting up viewpager and offset
         val v = ViewPagerAdapter(supportFragmentManager,lifecycle,fragments)
         val viewPager= findViewById<ViewPager2>(R.id.viewPager)
         viewPager.adapter = v
         viewPager.offscreenPageLimit = 1
+
+        // checking scroll behaviour
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                Log.d("scroll", "onPageScrolled: "+position)
+
+                if(position==0 && viewPager.scrollState == ViewPager2.SCROLL_STATE_DRAGGING){
+                    viewPager.setCurrentItem(2,true)
+                }
+                if(position==2 && viewPager.scrollState == ViewPager2.SCROLL_STATE_DRAGGING){
+                    viewPager.setCurrentItem(0,true)
+                }
+            }
+        })
+
+        // setting up tablayout with viewpager
         val tabLayout: TabLayout = findViewById(R.id.tablayout)
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = category[position]
