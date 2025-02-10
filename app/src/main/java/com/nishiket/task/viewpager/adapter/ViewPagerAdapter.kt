bcf8fc95086069da6a1ac3eca784e975a95e245b
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-10 12:25:08 +0530

    vertical and horizontal scroll is done, with all the images and adapter

---
+++ app/src/main/java/com/nishiket/task/viewpager/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,13 @@
+package com.nishiket.task.viewpager.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager,lifecycle: Lifecycle,val fragments: List<Fragment>): FragmentStateAdapter(fragmentManager,lifecycle) {
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position%3]
+
+}
