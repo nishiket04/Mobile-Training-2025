29c9d5b081b8758d840b9f21fa91ad82433363c4
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-13 11:06:49 +0530

    all the snackBar added

--- app/src/main/java/com/nishiket/task/snackbar_fab/SnackbarFabActivity.kt
+++ app/src/main/java/com/nishiket/task/snackbar_fab/SnackbarFabActivity.kt
@@ -2,14 +2,20 @@ package com.nishiket.task.snackbar_fab
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.nishiket.task.R
 
 class SnackbarFabActivity : AppCompatActivity() {
     private lateinit var addFab: FloatingActionButton
     private lateinit var likeFab: FloatingActionButton
     private lateinit var shareFab: FloatingActionButton
+    private lateinit var animatedSnackBar: Button
+    private lateinit var actionSnackBar: Button
+    private lateinit var anchorSnackBar: Button
 
     // flag to check fab icon is open or close
     private var fabFlab: Boolean = false
@@ -35,6 +41,41 @@ class SnackbarFabActivity : AppCompatActivity() {
             shareFab.visibility = View.GONE
             fabFlab = false // flag change to close
         }
+
+        // slide Animated SnackBar
+        animatedSnackBar.setOnClickListener {
+            Snackbar.make(
+                findViewById(R.id.main),
+                "This is animated snackBar",
+                Snackbar.LENGTH_LONG
+            ).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
+                .setBackgroundTint(resources.getColor(R.color.teal_200)).show()
+        }
+
+        // action button snackBar
+        actionSnackBar.setOnClickListener {
+            Snackbar.make(
+                findViewById(R.id.main),
+                "This is snackBar with action button",
+                Snackbar.LENGTH_LONG
+            ).setAction("Close", {
+                Snackbar.make(
+                    findViewById(R.id.main),
+                    "SnackBar is closed",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }).setTextColor(resources.getColor(R.color.teal_700))
+                .setBackgroundTint(resources.getColor(R.color.medium_black)).show()
+        }
+
+        // anchor to addFab snackBar
+        anchorSnackBar.setOnClickListener {
+            Snackbar.make(
+                findViewById(R.id.main),
+                "This is snackBar with anchor to fab",
+                Snackbar.LENGTH_LONG
+            ).setAnchorView(R.id.addFab).show()
+        }
     }
 
     //Function to find ids of the views
@@ -42,5 +83,8 @@ class SnackbarFabActivity : AppCompatActivity() {
         addFab = findViewById(R.id.addFab)
         likeFab = findViewById(R.id.likeFab)
         shareFab = findViewById(R.id.shareFab)
+        animatedSnackBar = findViewById(R.id.animatedSnackBar)
+        actionSnackBar = findViewById(R.id.actionSnackBar)
+        anchorSnackBar = findViewById(R.id.anchorSnackBar)
     }
 }
