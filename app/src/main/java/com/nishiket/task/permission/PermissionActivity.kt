0875d9656573d27b9c9594451a1552a725e601b2
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-19 12:50:38 +0530

    add button in main kt

---
+++ app/src/main/java/com/nishiket/task/permission/PermissionActivity.kt
@@ -0,0 +1,15 @@
+package com.nishiket.task.permission
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nishiket.task.R
+
+class PermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+    }
+}
