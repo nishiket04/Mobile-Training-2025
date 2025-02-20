8e48139b966bbfb1b789ab5c3836c3ac9baffa1d
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-20 13:43:45 +0530

    add button in main kt

---
+++ app/src/main/java/com/nishiket/task/webview/WebViewActivity.kt
@@ -0,0 +1,15 @@
+package com.nishiket.task.webview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nishiket.task.R
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
