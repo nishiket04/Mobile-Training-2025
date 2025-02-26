9deb4107948c87b6c99846c5436a68252ea5ee4b
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-26 12:53:32 +0530

    Api call on every 5 min

--- app/src/main/java/com/nishiket/task/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/nishiket/task/retrofit/RetrofitInstance.kt
@@ -6,12 +6,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
     private const val BASE_URL_POST = "https://fakestoreapi.com/"
+    private const val BASE_URL_COUNTER = "https://docs.google.com/"
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build()
     }
     fun getInstancePost(): Retrofit {
         return Retrofit.Builder().baseUrl(BASE_URL_POST)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+    fun getInstanceCounter(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL_COUNTER)
             .addConverterFactory(GsonConverterFactory.create()).build()
     }
 }
--- app/src/main/java/com/nishiket/task/service/CounterService.kt
+++ app/src/main/java/com/nishiket/task/service/CounterService.kt
@@ -2,16 +2,25 @@ package com.nishiket.task.service
 
 import android.app.Service
 import android.content.Intent
-import android.media.MediaPlayer
+import android.os.Handler
 import android.os.IBinder
-import android.provider.Settings
-import android.util.Log
+import android.os.Looper
 import androidx.core.app.NotificationCompat
 import com.nishiket.task.R
+import com.nishiket.task.retrofit.Retrofit
+import com.nishiket.task.retrofit.RetrofitInstance
+import com.nishiket.task.retrofit.Success
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class CounterService: Service() {
+class CounterService : Service() {
+    private var counter: Int = 0
+    private val retrofit: Retrofit =
+        RetrofitInstance.getInstanceCounter().create(Retrofit::class.java)
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        when(intent?.action){
+        when (intent?.action) {
             State.START.toString() -> start()
             State.STOP.toString() -> stopSelf()
         }
@@ -22,11 +31,32 @@ class CounterService: Service() {
         return null
     }
 
-    private fun start(){
-        val notification = NotificationCompat.Builder(this,"Api").setSmallIcon(R.mipmap.ic_launcher).setContentText("This is test").setContentTitle("Increasing Counter").build()
-        startForeground(1,notification)
+    private fun start() {
+        val notification =
+            NotificationCompat.Builder(this, "Api").setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("This is test").setContentTitle("Increasing Counter").build()
+        startForeground(1, notification)
+        Handler(Looper.getMainLooper()).post(object : Runnable {
+            override fun run() {
+                val call = retrofit.updateCounter("nishiket", counter.toString())
+                call.enqueue(object : Callback<Success> {
+                    override fun onResponse(p0: Call<Success>, p1: Response<Success>) {
+
+                    }
+
+                    override fun onFailure(p0: Call<Success>, p1: Throwable) {
+
+                    }
+
+                })
+                counter++
+                Handler(Looper.getMainLooper()).postDelayed(this, 5*60*1000)
+            }
+        })
+
     }
 }
-enum class State{
-    START,STOP;
+
+enum class State {
+    START, STOP;
 }
