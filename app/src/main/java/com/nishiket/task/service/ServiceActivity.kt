bd599a4e77fa5423ac7da2fe39685b6f137d6306
Nishiket Bhatt <nishiket04@gmail.com>
2025-02-26 11:09:34 +0530

    Ui

--- app/src/main/java/com/nishiket/task/service/ServiceActivity.kt
+++ app/src/main/java/com/nishiket/task/service/ServiceActivity.kt
@@ -1,6 +1,7 @@
 package com.nishiket.task.service
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,8 +9,18 @@ import androidx.core.view.WindowInsetsCompat
 import com.nishiket.task.R
 
 class ServiceActivity : AppCompatActivity() {
+    private lateinit var start:Button
+    private lateinit var stop:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+        // assign Ids using function
+        findId()
+    }
+
+    // Function to find ids of the views
+    fun findId(){
+        start = findViewById(R.id.start)
+        stop = findViewById(R.id.stop)
     }
 }
